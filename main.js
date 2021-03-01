630ae896674d6de9eb9472b0f227460800da034e
codemantra10 <73803525+codemantra10@users.noreply.github.com>
2021-02-28 19:54:41 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -20,10 +20,96 @@ function block_update(getImage){
     fabric.Image.fromURL(getImage,function(Img){
     block_object=Img;
     block_object.scaleToWidth(blockimagewidth);
-    block_object.scaletoHeight(blockimageheight);
+    block_object.scaleToHeight(blockimageheight);
     block_object.set({
     top:player_y,left:player_x
     });
     canvas.add(block_object);
     });
-}
+    }
+window.addEventListener("keydown",keyclicked); 
+    function keyclicked(e){
+    keypressed=e.keyCode;
+    console.log(keypressed);
+    if (keypressed=="65") {
+     block_update("captain_america_left_hand.png");
+     console.log("a");
+    };
+    if (keypressed=="66") {
+        block_update("hulk_face.png");
+        console.log("b");
+       };
+       if (keypressed=="67") {
+        block_update("hulk_left_hand.png");
+        console.log("c");
+       };   
+       if (keypressed=="68") {
+        block_update("hulk_legs.png");
+        console.log("d");
+       }; 
+       if (keypressed=="69") {
+        block_update("hulk_right_hand.png");
+        console.log("e");
+       };   
+       if (keypressed=="70") {
+        block_update("hulkd_body.png");
+        console.log("f");
+       };          if (keypressed=="71") {
+        block_update("ironman_body.png");
+        console.log("g");
+       };   
+       if (keypressed=="72") {
+        block_update("ironman_face.png");
+        console.log("h");
+       };   
+       if (keypressed=="73") {
+        block_update("ironman_legs.png");
+        console.log("i");
+       };
+       if (keypressed=="74") {
+        block_update("ironman_right_hand.png");
+        console.log("j");
+       };
+       if (keypressed=="75") {
+        block_update("spiderman_body.png");
+        console.log("k");
+       };
+        if (keypressed=="76") {
+            block_update("sdiperman_face.png");
+            console.log("l");
+}; 
+if (keypressed=="77") {
+    block_update("spiderman_legs.png");
+    console.log("m");
+   }; 
+   if (keypressed=="78") {
+    block_update("spiderman_right_hand.png");
+    console.log("n");
+   }; 
+   if (keypressed=="79") {
+    block_update("thor_face.png");
+    console.log("o");
+   }; 
+   if (keypressed=="80") {
+    block_update("thor_left_hand.png");
+    console.log("p");
+    if (keypressed=="81") {
+        block_update("thor_right_hand.png");
+        console.log("q");
+}; 
+};      
+       if (e.shiftKey==true && keypressed=="82"){
+       console.log("r")
+       blockimageheight=blockimageheight+10;
+       blockimagewidth=blockimagewidth+10;
+       document.getElementById("current_height").innerHTML=blockimageheight;
+       document.getElementById("current_width").innerHTML=blockimagewidth;
+       };  
+       if (e.shiftKey==true && keypressed=="83"){
+        console.log("s")
+        blockimageheight=blockimageheight-10;
+        blockimagewidth=blockimagewidth-10;
+        document.getElementById("current_height").innerHTML=blockimageheight;
+        document.getElementById("current_width").innerHTML=blockimagewidth;
+        };        
+};
